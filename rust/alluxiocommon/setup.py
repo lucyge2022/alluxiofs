325f1acdd168f8ab476655f0896828090abaa952
Lucy Ge <lucy.ge@alluxio.com>
2024-05-30 22:00:43 -0700

    try make alluxiocommon a submodule

--- rust/alluxiocommon/setup.py
+++ rust/alluxiocommon/setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
-from setuptools_rust import Binding
-from setuptools_rust import RustExtension
-
-
-setup(
-    name="alluxiocommon",
-    version="0.1",
-    rust_extensions=[RustExtension("alluxiocommon", binding=Binding.PyO3)],
-    packages=["alluxiocommon"],
-    # include any other necessary package metadata
-    zip_safe=False,
-)
+# from setuptools import setup
+# from setuptools_rust import Binding
+# from setuptools_rust import RustExtension
+#
+#
+# setup(
+#     name="alluxiocommon",
+#     version="1.0.1.dev1",
+#     rust_extensions=[RustExtension("alluxiofs.alluxiocommon.alluxiocommon", "alluxiofs/rust/alluxiocommon/Cargo.toml", binding=Binding.PyO3)],
+#     packages=["alluxiocommon"],
+#     # include any other necessary package metadata
+#     include_package_data=True,
+#     zip_safe=False,
+# )
